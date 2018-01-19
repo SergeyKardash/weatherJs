571ced23844d6ffb5b4c7688b8dbe0434c2bbfe2
Sergey Kardash <kardashsergeydev@gmail.com>
2018-01-19 16:51:32 +0200

    show weather

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 // Init Weather class
 const weather = new Weather('Boston', 'MA');
+// Init UI
+const ui = new UI;
 
 // DOM Loaded eventListener
 document.addEventListener('DOMContentLoaded', getWeather)
@@ -8,6 +10,7 @@ function getWeather(){
   weather.getWeather()
     .then(results => {
       console.log(results)
+      ui.paint(results)
     })
     .catch(err => {
       console.log(err)
